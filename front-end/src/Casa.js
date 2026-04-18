3b0c7446273c42c0dc7d19c3edc1464bf3c63571
lasuzukii <laurasuzukicodecode@gmail.com>
2026-04-18 20:03:17 -0300

    adiciona highlight verde ao arrastar peça sobre casa válida; remove dependência não usada de dicaAtiva

--- front-end/src/Casa.js
+++ front-end/src/Casa.js
@@ -1,23 +1,37 @@
 import { useDrop } from "react-dnd";
 
-export default function Casa({ i, j, children, moverPeca }) {
-  const [, drop] = useDrop(() => ({
+export default function Casa({ i, j, children, moverPeca, dicaAtiva }) {
+  const [{ isOver }, drop] = useDrop(() => ({
     accept: "PECA",
     drop: (item) => {
       moverPeca(item.posicao, [i, j]);
     },
+    collect: (monitor) => ({ isOver: !!monitor.isOver() }),
   }));
+
+  const isDark = (i + j) % 2 !== 0;
+
+  let bg;
+  if (isOver && isDark) {
+    bg = "#90c050";
+  } else if (isDark) {
+    bg = "#c08040";
+  } else {
+    bg = "#f0d0a0";
+  }
 
   return (
     <div
       ref={drop}
       style={{
-        width: 70,
-        height: 70,
-        background: (i + j) % 2 === 0 ? "#f0d9b5" : "#b58863",
+        width: "100%",
+        aspectRatio: "1",
+        background: bg,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        transition: "background 0.15s",
+        cursor: isDark ? "pointer" : "default",
       }}
     >
       {children}
